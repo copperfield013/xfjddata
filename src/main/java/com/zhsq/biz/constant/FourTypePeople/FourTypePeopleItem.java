dc68e097a1673e17ccf01752212558a9f335a7f0
hbliu <hbliu@47743ba2-fc1c-4d5f-a63d-743578d09b5e>
2019-01-26 10:12:19 +0000

    git-svn-id: http://47.100.187.235:13932/svn/xfjddata@42 47743ba2-fc1c-4d5f-a63d-743578d09b5e

---
+++ src/main/java/com/zhsq/biz/constant/FourTypePeople/FourTypePeopleItem.java
@@ -0,0 +1,15 @@
+package com.zhsq.biz.constant.FourTypePeople;
+
+public class FourTypePeopleItem {
+	public static final String 标签="XFJD1016";
+	public static final String 基本信息="XFJD1017";
+	public static final String 姓名="XFJD1018";
+	public static final String 所属社区="XFJD1019";
+	public static final String 四类人员="XFJD1020";
+	public static final String 更新时间="XFJD1032";
+	public static final String 身份证号="XFJD1180";
+	public static final String 四类人员转变状态="XFJD1190";
+	public static final String 编辑时间="XFJDE1015_ED";
+	
+	public static final String 就业形式="XFJD1264";
+}
